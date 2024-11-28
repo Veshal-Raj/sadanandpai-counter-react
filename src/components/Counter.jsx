d24fd513e2e1121af9553883090cc82c19086bdc
veshalsamaaro <veshal@samaaro.com>
2024-11-28 20:31:00 +0530

    add: counter functionlity completed

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { useState } from 'react'
+import '../App.css'
 
 const Counter = () => {
+    const [count, setCount] = useState(0);
+    const [inputVal, setInputVal] = useState(1);
+
+    const incCount = () => {
+        let newCount = count + inputVal;
+        setCount(newCount);
+    }
+
+    const decCount = () => {
+        let newCount = count - inputVal;
+        setCount(newCount);
+    }
+
+    const resetVal = () => {
+        setCount(0);
+        setInputVal(1);
+    }
+
+    const handleInputChange = (e) => {
+        const value = Number(e.target.value);
+        setInputVal(value)
+    }
+
   return (
-    <div>
-      counter
+    <div className='container'>
+        <div className="row first-row">
+            <p id='count'>{count}</p>
+            <button className="custom-btn" onClick={() => decCount()}>-</button>
+            <button className="custom-btn" onClick={() => incCount()}>+</button>
+        </div>
+        <div className="row">
+            <p>Increment/Decrement by: </p>
+            <input type="number" value={inputVal} onChange={handleInputChange} />
+        </div>
+        <div className="row">
+            <button className="custom-btn" onClick={() => resetVal()}>Reset</button>
+        </div>
     </div>
   )
 }
